2554f1de3b5b83eb8a2e0b1e44ee0c311501d436
cgm616 <cgm616@me.com>
2018-07-15 20:04:03 -0500

    Allow multiple timers in Delay and Timer
    TIMER1 and TIMER2 can now be used for this functionality instead of just
    TIMER0.

--- src/delay.rs
+++ src/delay.rs
@@ -1,82 +1,95 @@
 //! Delays
 
 use cast::u32;
-use nrf51::TIMER0;
+use nrf51::{TIMER0, TIMER1, TIMER2};
 
 use hal::blocking::delay::{DelayMs, DelayUs};
 
 /// System timer `TIMER0` as a delay provider
-pub struct Delay {
-    timer: TIMER0,
+pub struct Delay<TIM> {
+    timer: TIM,
 }
 
-impl Delay {
-    /// Configures the TIMER0 as a delay provider
-    pub fn new(timer: TIMER0) -> Self {
-        timer.tasks_stop.write(|w| unsafe { w.bits(1) });
+macro_rules! delay {
+    ($($TIM:ty),+) => {
+        $(
+            impl Delay<$TIM> {
+                /// Configures the TIMER0 as a delay provider
+                pub fn new(timer: $TIM) -> Self {
+                    timer.tasks_stop.write(|w| unsafe { w.bits(1) });
 
-        // Set counter to 24bit mode
-        timer.bitmode.write(|w| unsafe { w.bits(2) });
+                    // Set counter to 24bit mode
+                    timer.bitmode.write(|w| unsafe { w.bits(2) });
 
-        // Set prescaler to 4 == 1MHz timer
-        timer.prescaler.write(|w| unsafe { w.bits(4) });
+                    // Set prescaler to 4 == 1MHz timer
+                    timer.prescaler.write(|w| unsafe { w.bits(4) });
 
-        Delay { timer }
-    }
+                    Delay { timer }
+                }
 
-    pub fn free(self) -> TIMER0 {
-        self.timer
-    }
+                pub fn free(self) -> $TIM {
+                    self.timer
+                }
+            }
+
+            impl DelayMs<u32> for Delay<$TIM> {
+                fn delay_ms(&mut self, ms: u32) {
+                    self.delay_us(ms * 1_000);
+                }
+            }
+
+            impl DelayMs<u16> for Delay<$TIM> {
+                fn delay_ms(&mut self, ms: u16) {
+                    self.delay_ms(u32(ms));
+                }
+            }
+
+            impl DelayMs<u8> for Delay<$TIM> {
+                fn delay_ms(&mut self, ms: u8) {
+                    self.delay_ms(u32(ms));
+                }
+            }
+
+            impl DelayUs<u32> for Delay<$TIM> {
+                fn delay_us(&mut self, us: u32) {
+                    /* Clear event in case it was used before */
+                    self.timer.events_compare[0].write(|w| unsafe { w.bits(0) });
+
+                    /* Program counter compare register with value */
+                    self.timer.cc[0].write(|w| unsafe { w.bits(us) });
+
+                    /* Clear current counter value */
+                    self.timer.tasks_clear.write(|w| unsafe { w.bits(1) });
+
+                    /* Start counting */
+                    self.timer.tasks_start.write(|w| unsafe { w.bits(1) });
+
+                    /* Busy wait for event to happen */
+                    while self.timer.events_compare[0].read().bits() == 0 {}
+
+                    /* Stop counting */
+                    self.timer.tasks_stop.write(|w| unsafe { w.bits(1) });
+                }
+            }
+
+            impl DelayUs<u16> for Delay<$TIM> {
+                fn delay_us(&mut self, us: u16) {
+                    self.delay_us(u32(us))
+                }
+            }
+
+            impl DelayUs<u8> for Delay<$TIM> {
+                fn delay_us(&mut self, us: u8) {
+                    self.delay_us(u32(us))
+                }
+            }
+
+        )+
+    };
 }
 
-impl DelayMs<u32> for Delay {
-    fn delay_ms(&mut self, ms: u32) {
-        self.delay_us(ms * 1_000);
-    }
-}
-
-impl DelayMs<u16> for Delay {
-    fn delay_ms(&mut self, ms: u16) {
-        self.delay_ms(u32(ms));
-    }
-}
-
-impl DelayMs<u8> for Delay {
-    fn delay_ms(&mut self, ms: u8) {
-        self.delay_ms(u32(ms));
-    }
-}
-
-impl DelayUs<u32> for Delay {
-    fn delay_us(&mut self, us: u32) {
-        /* Clear event in case it was used before */
-        self.timer.events_compare[0].write(|w| unsafe { w.bits(0) });
-
-        /* Program counter compare register with value */
-        self.timer.cc[0].write(|w| unsafe { w.bits(us) });
-
-        /* Clear current counter value */
-        self.timer.tasks_clear.write(|w| unsafe { w.bits(1) });
-
-        /* Start counting */
-        self.timer.tasks_start.write(|w| unsafe { w.bits(1) });
-
-        /* Busy wait for event to happen */
-        while self.timer.events_compare[0].read().bits() == 0 {}
-
-        /* Stop counting */
-        self.timer.tasks_stop.write(|w| unsafe { w.bits(1) });
-    }
-}
-
-impl DelayUs<u16> for Delay {
-    fn delay_us(&mut self, us: u16) {
-        self.delay_us(u32(us))
-    }
-}
-
-impl DelayUs<u8> for Delay {
-    fn delay_us(&mut self, us: u8) {
-        self.delay_us(u32(us))
-    }
+delay! {
+    TIMER0,
+    TIMER1,
+    TIMER2
 }
